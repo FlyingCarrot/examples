612e7719d881a23e25939aa6cf74e0c1d5a36ca3
FlyingCarrot <herowudiluobo@qq.com>
2018-09-09 21:13:30 +0800

    go

--- dcgan/main.py
+++ dcgan/main.py
@@ -18,7 +18,7 @@ from collections import OrderedDict
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', required=True, help='cifar10 | lsun | imagenet | folder | lfw | fake')
 parser.add_argument('--dataroot', required=True, help='path to dataset')
-parser.add_argument('--checkpoints_dir', require=True)
+parser.add_argument('--checkpoints_dir', required=True)
 parser.add_argument('--workers', type=int, help='number of data loading workers', default=2)
 parser.add_argument('--batchSize', type=int, default=64, help='input batch size')
 parser.add_argument('--imageSize', type=int, default=64, help='the height / width of the input image to network')
